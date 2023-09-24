99236f3f0af1cc2c293ea9bc790ad7fad6a2c99a
Avinash Prasad <avinashprasad4608@gmail.com>
2023-09-24 13:23:21 +0530

    change cors

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ config({
 //Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: `http://localhost:5173`  ||  process.env.FRONTEND_URI,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 // app.use(ErrorHandler());
 
 //Using routes
